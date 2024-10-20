67e7c6b9c7fdd521b79d18df0042240fb37a33f3
Abdallah Hassan <abdohassan2000@hotmail.com>
2024-10-20 12:24:21 +0300

    ..

--- app/src/main/java/com/example/steamdbmockup/ui/presentation/TopRatedGamesScreen/TopRatedGamesScreen.kt
+++ app/src/main/java/com/example/steamdbmockup/ui/presentation/TopRatedGamesScreen/TopRatedGamesScreen.kt
@@ -1,12 +1,25 @@
 package com.example.steamdbmockup.ui.presentation.TopRatedGamesScreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.recipescompose.presentation.components.ColumnGameList
+import com.example.steamdbmockup.R
+import com.example.steamdbmockup.common.TextTitle
+import com.example.steamdbmockup.ui.theme.Grey2
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopRatedGamesScreen(
     navController: NavController,
@@ -16,7 +29,27 @@ fun TopRatedGamesScreen(
     val loading = viewModel.topRatedGamesLoading.value
     val listState = rememberLazyListState()
 
-    Scaffold { it ->
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    TextTitle(
+                        text = stringResource(id = R.string.top_rated_games),
+                        modifier = Modifier
+                            .padding(10.dp),
+                        fontSize = 20,
+                        color = Color.White
+                    )
+                },
+                modifier = Modifier
+                    .background(Grey2),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Grey2
+
+                )
+            )
+        }
+    ) { it ->
         ColumnGameList(
             loading = loading,
             games = topRatedGames,
--- app/src/main/java/com/example/steamdbmockup/ui/presentation/TopThisYear/TopThisYearScreen.kt
+++ app/src/main/java/com/example/steamdbmockup/ui/presentation/TopThisYear/TopThisYearScreen.kt
@@ -2,13 +2,26 @@ package com.example.steamdbmockup.ui.presentation.TopThisYear
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.recipescompose.presentation.components.ColumnGameList
+import com.example.steamdbmockup.R
+import com.example.steamdbmockup.common.TextTitle
+import com.example.steamdbmockup.ui.theme.Grey2
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TopThisYearScreen(
@@ -19,7 +32,27 @@ fun TopThisYearScreen(
     val loading = viewModel.topThisYearGamesLoading.value
     val listState = rememberLazyListState()
 
-    Scaffold { it ->
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    TextTitle(
+                        text = stringResource(id = R.string.top_this_year),
+                        modifier = Modifier
+                            .padding(10.dp),
+                        fontSize = 20,
+                        color = Color.White
+                    )
+                },
+                modifier = Modifier
+                    .background(Grey2),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Grey2
+
+                )
+            )
+        }
+    ) { it ->
         ColumnGameList(
             loading = loading,
             games = topThisYearGames,
